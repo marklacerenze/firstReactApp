91ce41b7ac6783cb4f07fdc7de52c8658d4b9fdd
Mark Lacerenze <marklacer@gmail.com>
2021-02-10 16:29:39 -0300

    Counting app in react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Contador from './components/Contador'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Titulo de nuestro primer proyecto</h1>
+      <Contador></Contador>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hola.jsx
@@ -0,0 +1,13 @@
+// Los componentes SIEMPRE se declaran con jsx
+// JSX es la renderización de componentes en HTML
+import React from 'react'
+
+function Hola(){
+    return(
+        <div>
+            <h2>Primer componente con JSX</h2>   
+        </div>
+    )
+}
+
+export default Hola;
