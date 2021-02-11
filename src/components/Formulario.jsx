193d6eed30e4fe7bc8aa29b206973729804846fc
Mark Lacerenze <marklacer@gmail.com>
2021-02-11 14:15:24 -0300

    Form using boostrarp

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,48 @@
 import React, {useState} from 'react'
 
 const Formulario = () =>{
+
+    const[nombre, setNombre] = useState(" ");
+    const[edad, setEdad] = useState(" ");
+    
+    // Capturo el evento submit por parametro
+    const Validar = (event) => {
+        event.preventDefault()
+        console.log("Ha pulsado el boton");
+        // Compruebo si el nombre tiene datos
+        if(!nombre.trim()){
+            console.log("El nombre está vacío");
+            return;
+        }
+        if(!edad.trim()){
+            console.log("La edad está vacía");
+            return;
+        }
+    }
+
     return(
         <div className="container">
-            <form className="form-group">
-                <input placeholder="Nombre" type="text" className="form-control mb-3"></input>
-                <input placeholder="Edad" type="text" className="form-control mb-3"></input>
-                <input type="submit" className="btn btn-info btn-block"></input>
+            <form onSubmit={Validar} className="form-group">
+                <input 
+                    placeholder="Nombre" 
+                    type="text" 
+                    className="form-control mb-3"
+                    // Capturo el nombre
+                    onChange={(e) => {setNombre(e.target.value)}}
+                    />
+                
+                <input
+                    placeholder="Edad" 
+                    type="text"
+                    className="form-control mb-3" 
+                    // Capturo la edad
+                    onChange={(e) => {setEdad(e.target.value)}}
+                    />
+
+                <input 
+                    type="submit" 
+                    className="btn btn-info btn-block"/>
+
             </form>
         </div>
     )
